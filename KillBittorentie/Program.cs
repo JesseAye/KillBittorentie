6c4031a8f219d32995b14ee7423d615e24e1a9d3
Jesse Ayala <jesserayala95@gmail.com>
2022-03-11 14:22:21 -0500

    Add project files.

---
+++ KillBittorentie/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KillBittorentie
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool BitTorrentStarted = false;
+
+			while (true)
+			{
+				if (!BitTorrentStarted)
+				{
+					if (Process.GetProcessesByName("BitTorrent.exe") != null)
+					{
+						BitTorrentStarted = true;
+						Thread.Sleep(1000);
+					}
+				}
+
+				else
+				{
+					if (Process.GetProcessesByName("BitTorrent.exe") != null)
+					{
+						Process[] _processes = null;
+						_processes = Process.GetProcessesByName("bittorrentie");
+
+						foreach (Process process in _processes)
+						{
+							if (!process.HasExited)
+							{
+								process.Kill();
+								Console.WriteLine($"Killed: {process.ProcessName} PID {process.Id}");
+							}
+						}
+					}
+
+					else
+					{
+						BitTorrentStarted = false;
+						Thread.Sleep(1000);
+					}
+				}
+			}
+		}
+	}
+}
